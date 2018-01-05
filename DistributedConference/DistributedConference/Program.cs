00a8da62a0a7855e3b3e174abd79ba0d9e160d9c
jaxe1234 <jacobbruuns@hotmail.com>
2018-01-05 16:05:29 +0100

    Fixed the naming scheme for spaces in RepoUtility

--- DistributedConference/DistributedConference/Program.cs
+++ DistributedConference/DistributedConference/Program.cs
@@ -13,7 +13,7 @@ namespace DistributedConference
     {
         static void Main(string[] args)
         {
-            string uri = Dns.GetHostByName(Dns.GetHostName()).AddressList[0].ToString() + ":5002";
+            string uri = "tcp://" + Dns.GetHostByName(Dns.GetHostName()).AddressList[0].ToString() + ":5002";
 
             SpaceRepository spaceRepo = new SpaceRepository();
             new ChatApp.ChatTest(args, spaceRepo, uri);
