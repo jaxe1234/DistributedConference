170664a1dc536944ab76c7dd577d97195d90087e
s164395 <s164395@student.dtu.dk>
2018-01-05 15:20:06 +0100

    Created utility for space naming and image processing constructor

---
+++ DistributedConference/ConferenceImageProcessing/ConferenceImageSender.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dotSpace.BaseClasses;
+using dotSpace.Enumerations;
+using dotSpace.Interfaces;
+using dotSpace.Interfaces.Space;
+using dotSpace.Objects;
+using dotSpace.Objects.Network;
+using DistributedConference;
+
+namespace ConferenceImageProcessing
+{
+    class ConferenceImageSender
+    {
+        private static readonly string uri = "tcp://127.0.0.1:5001";
+
+        private ISpace Space { get; set; }
+        public string ConferenceName { get; private set; }
+
+        public ConferenceImageSender(string name, SpaceRepository repo)
+        {
+            ConferenceName = name.Replace(" ", "");
+            Space = new RemoteSpace(RepoUtility.GenerateUniqueSpaceUri(uri, name));
+            repo.AddSpace(ConferenceName + "ImageStream", Space);
+
+        }
+        
+
+
+    }
+}
