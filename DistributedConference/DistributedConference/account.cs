974ec4f04b7a999e29cfe27c46bc6804f9ec6f8c
s164434 <s164434@student.dtu.dk>
2018-01-05 12:37:45 +0100

    account and login classes

---
+++ DistributedConference/DistributedConference/account.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace DistributedConference
+{
+    class account
+    {
+        public string username { get; }
+        public byte[] salt { get;}
+        public string hash { get; }
+
+       
+
+        public account(string username, string password)
+        {
+            
+
+            this.username = username;
+            this.salt = getSalt();
+            this.hash = generatePassHash(Encoding.UTF8.GetBytes(password), salt);
+            
+            
+
+
+        }
+
+        private string generatePassHash(byte[] password, byte[] salt)
+        {
+            byte[] data = new byte[password.Length + salt.Length];
+            password.CopyTo(data, 0);
+            salt.CopyTo(data, password.Length);
+            SHA512Managed hasher = new SHA512Managed();
+            return hasher.ComputeHash(data).ToString();
+            
+           
+        }
+
+        private byte[] getSalt()
+        {
+
+            byte[] saltInitArr = new byte[16];
+            RNGCryptoServiceProvider saltGen = new RNGCryptoServiceProvider();
+            saltGen.GetBytes(saltInitArr);
+            saltGen.Dispose();
+            return saltInitArr;
+            
+            
+
+
+        }
+    }
+}
