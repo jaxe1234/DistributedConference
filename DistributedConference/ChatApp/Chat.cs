dec2a51194572baca7e716159ad2d00af63f158d
jaxe1234 <jacobbruuns@hotmail.com>
2018-01-05 12:06:35 +0100

    First chat implememtation!

--- DistributedConference/ChatApp/Chat.cs
+++ DistributedConference/ChatApp/Chat.cs
@@ -8,5 +8,19 @@ namespace ChatApp
 {
     public class Chat
     {
+        private SpaceRepository ChatSpace;
+        String uri = "tcp://127.0.0.1:5001";
+        private String ConferenceName = "room";
+        public Chat(bool isHost)
+        {
+            if (isHost)
+            {
+                ChatSpace = new SpaceRepository();
+            }
+            else
+            {
+                ChatSpace = new RemoteRepository(uri + "/" + ConferenceName);
+            }
+        }
     }
 }
