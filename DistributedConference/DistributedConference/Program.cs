ea52e86dcf0e131b48b2fc03129ca9fa2e0c3873
asger.moberg@gmail.com <asger.moberg@gmail.com>
2018-01-08 13:15:15 +0100

    Added PdfRasterizerService and cleaned file tree

--- DistributedConference/DistributedConference/Program.cs
+++ DistributedConference/DistributedConference/Program.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using PdfHandler;
+using System.Drawing;
+using System.Net;
 
 namespace DistributedConference
 {
@@ -11,7 +13,16 @@ namespace DistributedConference
     {
         static void Main(string[] args)
         {
-            new ChatApp.ChatTest(args);
+            var url = "https://meltdownattack.com/meltdown.pdf";
+            var client = new WebClient();
+            using (var stream = client.OpenRead(url))
+            {
+                IList<Image> images = new List<Image>();
+                PdfRasterizerService.GetImages(stream, ref images);
+                images = images;
+            }
+
+            //new ChatApp.ChatTest(args);
         }
     }
 }
