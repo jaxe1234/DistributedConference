d3b35dee3258b21b1d737912b252a6a73abd3c0f
jaxe1234 <jacobbruuns@hotmail.com>
2018-01-08 15:25:20 +0100

    Added a chatTest interface

--- DistributedConference/ConferenceLobbyUI/Program.cs
+++ DistributedConference/ConferenceLobbyUI/Program.cs
@@ -16,7 +16,7 @@ namespace ConferenceLobbyUI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new ChatUI());
+            Application.Run(new ConferenceUI());
         }
     }
 }
