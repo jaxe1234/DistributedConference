14a4147b483b88f8c6c680a37f65312f6bb8ddd4
s164395 <s164395@student.dtu.dk>
2018-01-18 13:17:43 +0100

    Fixed LoginServer not being able to launch. Apparently LoginServerTests was important

---
+++ DistributedConference/LoginServer/LoginServerLauncher.cs
@@ -0,0 +1,20 @@
+﻿using dotSpace.Objects.Network;
+using dotSpace.Objects.Space;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginServer
+{
+    class LoginServerTests
+    {
+        public static void Main(string[] args)
+        {
+            LoginServer testServer = new LoginServer();
+            var accountSpace = new RemoteSpace("tcp://" + _Resources.Resources.InternetProtocolAddress + ":5001/accountCreation");
+            var loginSpace = new RemoteSpace("tcp://" + _Resources.Resources.InternetProtocolAddress + ":5001/loginAttempts");
+        }
+    }
+}
